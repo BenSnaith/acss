025c6e6d9c90d6c96e6895d792b5c70d2168a84a
BenSnaith <bsnaith.bs@icloud.com>
2025-09-25 23:48:41 +0000

    chore: rand

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
-import {IndexComponent} from './index/index.component';
+import {IndexComponent} from './pages/index/index.component';
+import {EventsComponent} from './pages/events/events.component';
+import {CommitteeComponent} from './pages/committee/committee.component';
+import {SponsorComponent} from './pages/sponsor/sponsor.component';
+import {AboutComponent} from './pages/about/about.component';
 
 export const routes: Routes = [
-  { path: '', component: IndexComponent }
+  { path: '', component: IndexComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'committee', component: CommitteeComponent },
+  { path: 'sponsor', component: SponsorComponent },
+  { path: 'about', component: AboutComponent },
 ];
